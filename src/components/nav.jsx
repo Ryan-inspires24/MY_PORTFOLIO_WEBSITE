ada7e6c995eade5116d165fd800483f88754dbb7
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2026-07-20 11:43:43 +0100

    Installed react router dom and inserted the routes in my project

---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+//Navigation Component for the Homepage, to be inserted horizontally at the top of the page.
+export default function Navigation() {
+    return (
+        <div className="nav-container">
+            
+            <ul>
+                <li className="nav-item">Home</li>
+                <li className="nav-item">About</li>
+                <li className="nav-item">Projects</li>
+                <li className="nav-item">Content/Creator</li>
+            </ul>
+        </div>
+
+    )
+}
---
+++ src/pages/landing.jsx
@@ -0,0 +1,10 @@
+//Landing page that contains all the sections of the website.
+
+import React from "react"
+export default function HomePage(){
+    return(
+        <>
+        </>
+    )
+
+}
